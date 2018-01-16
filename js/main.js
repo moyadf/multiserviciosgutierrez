0c4a167e5e782b3fd739310661a509f2e7fc4f9d
David Fernando <damoso20.14@gmail.com>
2018-01-15 20:47:37 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".slide-1").slick({
+        asNavFor: '.slide-1',
+        arrows: false,
+        dots: false,
+        centerMode: false,
+        focusOnSelect: true
+    });
+});
